d709f6e003963a5fef3dfa3baaf8209d8d61735b
anima <animapandey2005@gmail.com>
2025-06-09 11:06:31 +0530

    1st commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'; // ← ✅ Add this line here
+import './index.css'; // This must be imported!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
